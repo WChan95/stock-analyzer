0bd0e311b83eae524504176ae91be6678d618302
WChan95 <59454205+WChan95@users.noreply.github.com>
2022-05-02 15:13:19 -0700

    Basic algorithm works but catching a lot of false positive

--- src/stock-parsing.js
+++ src/stock-parsing.js
@@ -2,25 +2,38 @@
 const fs = require("fs");
 const path = require("path");
 
-const data = require("/Users/soejun/Development/stock-analyzer/src/wsb_comment.json");
-const nyse = require("/Users/soejun/Development/stock-analyzer/src/nyse_tickers.json")
+const data = require("/Users/soejun/Development/stock-analyzer/src/wsb_comments.json");
+const nyse = require("/Users/soejun/Development/stock-analyzer/src/nyse_tickers.json");
+const num_tickers = Object.keys(nyse).length;
 
 const posts_length = Object.keys(data).length;
-let amzn_count = 0;
 
-/*  for (let j = 0; j < posts_length; j++) {
+/*Initializes nyse ticker list as object*/
+/*If a ticker has 0 counts, ticker will not be shown*/
+let stocks = [];
+for (let k = 0; k < num_tickers; k++) {
+  let ticker = { ticker: nyse[k], mentioned: 0 };
+  stocks.push(ticker);
+}
+
+for (let j = 0; j < posts_length; j++) {
   let comments = data[j];
   let comments_length = Object.keys(data[j]).length;
   for (let i = 0; i < comments_length; i++) {
     let comment = comments[i];
-    if (comment.includes("AAPL")) {
-      console.log(comment);
-      amzn_count++;
+    for (let k = 0; k < num_tickers; k++) {
+      if (comment.includes(stocks[k].ticker)) {
+        stocks[k].mentioned++;
+      }
     }
   }
-}  */
+}
 
+for(let i = 0; i < stocks.length; i++){
+    if(stocks[i].mentioned > 0){
+        console.log(stocks[i])
+    }
+}
 
-
-
-console.log(nyse);
+//what we need to do next is check if a substring of this comment matches any of the tickers in NYSE
+//if mentioned, create an object w/ that count
