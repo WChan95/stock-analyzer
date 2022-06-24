acdd6fa95e64b0599898452959825c808a174ca0
WChan95 <59454205+WChan95@users.noreply.github.com>
2022-06-24 12:24:15 -0700

    Added NLP word stop remover

--- src/stock-parsing.js
+++ src/stock-parsing.js
@@ -1,41 +1,50 @@
 //stock-parsing.js
-const fs = require("fs");
-const path = require("path");
 
+const stopwords = require("/Users/soejun/Development/stock-analyzer/src/stopwords.js");
 const data = require("/Users/soejun/Development/stock-analyzer/src/wsb_comments.json");
 const nyse = require("/Users/soejun/Development/stock-analyzer/src/nyse_tickers.json");
 const num_tickers = Object.keys(nyse).length;
-
 const posts_length = Object.keys(data).length;
 
-/*Initializes nyse ticker list as object*/
-/*If a ticker has 0 counts, ticker will not be shown*/
-let stocks = [];
-for (let k = 0; k < num_tickers; k++) {
-  let ticker = { ticker: nyse[k], mentioned: 0 };
-  stocks.push(ticker);
+function remove_stopwords(str) {
+  res = [];
+  words = str.split(" ");
+  for (i = 0; i < words.length; i++) {
+    word_clean = words[i].split(".").join("");
+    if (!stopwords.Array.includes(word_clean.toLowerCase())) {
+      res.push(word_clean);
+    }
+  }
+  return res.join(" ");
 }
+
+const cleaned_comments = [];
+const parsed_stocks = [];
 
 for (let j = 0; j < posts_length; j++) {
   let comments = data[j];
   let comments_length = Object.keys(data[j]).length;
   for (let i = 0; i < comments_length; i++) {
     let comment = comments[i];
-    for (let k = 0; k < num_tickers; k++) {
-
-        //Need to add in natural language processing to filter more accurately;
-      if (comment.includes(stocks[k].ticker)) {
-        stocks[k].mentioned++;
-      }
-    }
+    cleaned_comments.push(remove_stopwords(comment));
   }
 }
 
-for (let i = 0; i < stocks.length; i++) {
-  if (stocks[i].mentioned > 0) {
-    console.log(stocks[i]);
+for (let i = 0; i < cleaned_comments.length; i++) {
+  const regEx = new RegExp("[A-Zs]+");
+  comment = cleaned_comments[i];
+  let stock_tick = { ticker: "", count: 0 };
+  let potential_stock = comment.match(regEx);
+  if (potential_stock !== null && !parsed_stocks.includes(potential_stock[0])) {
+    /* stock_tick["ticker"] = potential_stock[0]; */
+    parsed_stocks.push(potential_stock[0]);
   }
 }
+for (let i = 0; i < parsed_stocks.length; i++) {
+  let ticker = parsed_stocks[i];
+  let stock = { ticker: ticker, count: 0 };
+  parsed_stocks[i] = stock;
+  
+}
 
-//what we need to do next is check if a substring of this comment matches any of the tickers in NYSE
-//if mentioned, create an object w/ that count
+console.log(parsed_stocks)
